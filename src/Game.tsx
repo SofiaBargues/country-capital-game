2d9db3d3d05444cb826790a04c44d00c9576b17b
Sofia Bargues <barguessofia@gmail.com>
2024-08-04 14:48:32 +0100

    butons

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export function Game({ data }: { data: Record<string, string> }) {
   const arrData = Object.entries(data).flat();
   const shuffledArray = arrData.sort(() => 0.5 - Math.random());
+  let [selected, setSelected] = useState("");
+  let [selected1, setSelected1] = useState("");
+
+  function handleClick(e) {
+    if (selected == "") {
+      setSelected(e.target.innerText);
+    } else {
+      setSelected1(e.target.innerText);
+    }
+  }
+
   return (
     <div>
       <div
@@ -8,7 +21,10 @@ export function Game({ data }: { data: Record<string, string> }) {
      flex gap-4 flex-wrap p-11"
       >
         {shuffledArray.map((name) => (
-          <button className="flex p-2 px-4 rounded-sm bg-slate-200 border border-black">
+          <button
+            onClick={handleClick}
+            className="flex p-2 px-4 rounded-md bg-slate-200 border border-black"
+          >
             {name}
           </button>
         ))}
