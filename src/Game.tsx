8813efc280154c3c22e80f53a9ceae71644480bd
Sofia Bargues <barguessofia@gmail.com>
2025-06-10 09:39:45 +0100

    time out

--- src/Game.tsx
+++ src/Game.tsx
@@ -10,8 +10,34 @@ for (const [key, val] of Object.entries(DATA)) {
 export function Game() {
   const [allNames, setAllNames] = useState(names);
   const [pair, setPair] = useState(["", ""]);
+  const [first, second] = pair;
+
   console.log(pair);
   console.log(allNames);
+
+  useEffect(() => {
+    console.log("useEffect > enter");
+    if (first === "") return;
+
+    const timer = setTimeout(() => {
+      console.log("running");
+
+      if (
+        (first in DATA && DATA[first] === second) ||
+        (second in DATA && DATA[second] === first)
+      ) {
+        const newAllNames = [...allNames];
+        setAllNames(newAllNames.filter((x) => x !== first && x !== second));
+        setPair(["", ""]);
+      } else if (first && second) {
+        setPair(["", ""]);
+      }
+    }, 2000);
+    return () => {
+      console.log("useEffect > clean up");
+      clearTimeout(timer)
+    };
+  }, [second, first, allNames]);
 
   function handleClick(name: string) {
     // let newPair = pair;
@@ -24,24 +50,13 @@ export function Game() {
       second = name;
       setPair([first, second]);
     }
-
-    setTimeout(() => {
-      if (
-        (first in DATA && DATA[first] === second) ||
-        (second in DATA && DATA[second] === first)
-      ) {
-        const newAllNames = [...allNames];
-        setAllNames(newAllNames.filter((x) => x !== first && x !== second));
-      } else if (first && second) {
-        setPair(["", ""]);
-      }
-    }, 2000);
   }
 
   return (
     <main>
       {allNames.map((name: string) => (
         <button
+          key={name}
           className={pair[0] === name || pair[1] === name ? "bg-slate-400" : ""}
           onClick={() => handleClick(name)}
         >
