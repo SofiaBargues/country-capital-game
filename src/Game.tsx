cf61737376ecdc76ba048870fa32bfcf1fe50f23
Sofia Bargues <barguessofia@gmail.com>
2025-06-10 09:49:47 +0100

    refactot

--- src/Game.tsx
+++ src/Game.tsx
@@ -16,39 +16,29 @@ export function Game() {
   console.log(allNames);
 
   useEffect(() => {
-    console.log("useEffect > enter");
-    if (first === "") return;
+    if (first === "" || second === "") return;
 
     const timer = setTimeout(() => {
-      console.log("running");
-
       if (
         (first in DATA && DATA[first] === second) ||
         (second in DATA && DATA[second] === first)
       ) {
-        const newAllNames = [...allNames];
-        setAllNames(newAllNames.filter((x) => x !== first && x !== second));
-        setPair(["", ""]);
-      } else if (first && second) {
-        setPair(["", ""]);
+        setAllNames(allNames.filter((x) => x !== first && x !== second));
       }
+      setPair(["", ""]);
     }, 2000);
     return () => {
-      console.log("useEffect > clean up");
-      clearTimeout(timer)
+      clearTimeout(timer);
     };
   }, [second, first, allNames]);
 
   function handleClick(name: string) {
-    // let newPair = pair;
-    let [first, second] = [...pair];
+    const [first, second] = [...pair];
 
     if (first === "") {
-      first = name;
-      setPair([first, second]);
+      setPair([name, second]);
     } else if (second === "") {
-      second = name;
-      setPair([first, second]);
+      setPair([first, name]);
     }
   }
 
