c1cfb909d9c80cde5257daf45548eb178feab907
Sofia Bargues <barguessofia@gmail.com>
2025-07-07 11:18:11 +0100

    states

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,57 +1,42 @@
 import { useEffect, useState } from "react";
 import DATA from "./data";
 
-function isMatch(first: string, second: string) {
-  if (DATA[first] === second || DATA[second] === first) return true;
-  else return false;
+function isMatch(pair: string[]) {
+  const [first, second] = pair;
+  if (first != "" && (DATA[second] === first || DATA[first] === second)) {
+    return true;
+  }
+  return false;
 }
 
 export function Game() {
-  const [names, setNames] = useState([...Object.entries(DATA).flat()]);
+  const [names, setNames] = useState(Object.entries(DATA).flat());
   const [pair, setPair] = useState(["", ""]);
 
-  useEffect(() => {
-    if (pair[0] === "" || pair[1] === "") return;
-    const timer = setTimeout(() => {
-      if (isMatch(pair[0], pair[1])) {
-        const first = pair[0];
-        const second = pair[1];
-        let newNames = [...names];
-        newNames = newNames.filter((x) => x != first && x != second);
-        setNames(newNames);
-      }
-      setPair(["", ""]);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [names, pair]);
-
-  function handleClick(x: string) {
-    console.log(x);
-    if (pair[0] === "") {
-      const newPair = [x, ""];
-      setPair(newPair);
-    } else if (pair[1] === "") {
+  // console.log(isMatch(pair), pair);
+  function handleClick(place: string) {
+    const [first, second] = [...pair];
+    
+    if (first === "") {
+      setPair([place, ""]);
+    } else if (second === "") {
       const newPair = [...pair];
-      newPair[1] = x;
+      newPair[1] = place;
       setPair(newPair);
+      if (isMatch(newPair)) {
+        const newNames = [...names].filter(
+          (x) => x != newPair[0] && x != newPair[1]
+        );
+        setNames(newNames);
+        console.log(newNames);
+        setPair(["", ""]);
+      }
     }
   }
-
-  const color =
-    pair[0] != "" && pair[1] === ""
-      ? "bg-slate-300"
-      : isMatch(pair[1], pair[0])
-      ? "bg-green-600"
-      : "bg-red-600";
-
   return (
     <>
-      {names.map((x, i) => (
-        <button
-          className={pair[0] === x || pair[1] === x ? color : ""}
-          key={i}
-          onClick={() => handleClick(x)}
-        >
+      {names.map((x) => (
+        <button onClick={() => handleClick(x)} key={x} className="p-2">
           {x}
         </button>
       ))}
